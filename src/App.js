50e32e79d0bd866b9710e6e3d308e0dcbf1591f9
sepinaco <sepinaco@gmail.com>
2021-10-21 21:14:42 +0200

    cloudinary added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+import {CloudinaryContext, Image, Video, Transformation } from 'cloudinary-react';
+
+function ImageAndVideo() {
+  useEffect(()=>{
+    const image = document.getElementById('imagencita');
+    const video = document.getElementById('videocito');
+    console.log(image)
+    console.log(video)
+  })
+  return null;
+}
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HOLI</h1>
+
+      <CloudinaryContext cloudName="sepinaco">
+        <div>
+          <Image publicId="sample" width="50" />
+        </div>
+        <Image id="imagencita" publicId="sample" width="0.5" />
+
+        <Video id="videocito" publicId="samples/sea-turtle" controls={true} style={{width:'50%'}} >
+          <Transformation width="0.4" angle="20" />
+        </Video>
+
+        {/* <Video cloudName="demo" publicId="dog" controls="true" >
+          <Transformation width="0.4" angle="20" />
+          <Transformation overlay="cloudinary_icon_white" width="60" opacity="50" gravity="south_east" y="15" x="60" />
+        </Video> */}
+
+      </CloudinaryContext>
+
+      <ImageAndVideo />
+
     </div>
   );
 }
