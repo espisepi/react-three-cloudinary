4a66e9c40780e9e883bbd12bfb404daaf5821f93
sepinaco <sepinaco@gmail.com>
2021-10-30 16:42:56 +0200

    testing change video

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import './App.css';
 import {CloudinaryContext, Image, Video, Transformation } from 'cloudinary-react';
 
 import { Scene1Canvas } from './scenes/scene1/Scene1';
+import { useCallback } from 'react/cjs/react.development';
 
+const dataMusic = [
+  {
+    url: 'video.mp4',
+  },
+  {
+    url: 'https://video-dl-esp.herokuapp.com/video/video?url=https://www.youtube.com/watch?v=96h97kNEgXM',
+  },
+  {
+    url: 'https://video-dl-esp.herokuapp.com/video/video?url=https://www.youtube.com/watch?v=BgcFGo7X3_M&list=PLbF25hg0V3wBCMH8pvbGOUw5fxp0pbrLA&index=71',
+  },
+]
 
 function App() {
+
+  const [index, setIndex] = useState(0);
+  const incrementIndex = useCallback(()=>{
+    const newIndex = (index + 1) % 3;
+    console.log(newIndex)
+    setIndex((v)=>(newIndex));
+  },[index]);
+
+  console.log(dataMusic[index])
 
   return (
     <div className="App">
@@ -23,7 +44,8 @@ function App() {
 
         {/* <video id="video" src="video.mp4" controls={true}></video> */}
 
-        <video id="video" src="https://video-dl-esp.herokuapp.com/video/video?url=https://www.youtube.com/watch?v=96h97kNEgXM" controls={true} crossOrigin="anonymous"></video>
+        {/* <video id="video" src="https://video-dl-esp.herokuapp.com/video/video?url=https://www.youtube.com/watch?v=96h97kNEgXM" controls={true} crossOrigin="anonymous"></video> */}
+        <video id="video" src={dataMusic[index].url} controls={true} crossOrigin="anonymous"></video>
 
         {/* <Video id="video" cloudName="demo" publicId="dog" controls="true" >
           <Transformation width="0.4" angle="20" />
@@ -35,6 +57,8 @@ function App() {
 
       <h1>Sepinaco</h1>
       <Scene1Canvas />
+
+      <button style={{position:'absolute', width:'100px', height:'100px'}} onClick={incrementIndex}>Cambiar cancion</button>
 
     </div>
   );
