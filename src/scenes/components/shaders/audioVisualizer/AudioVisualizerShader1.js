4bdd745ef80e9ad8c229e576ad7363fd923b7c96
sepinaco <sepinaco@gmail.com>
2021-11-06 21:51:43 +0100

    AudioVisualizerShader works with orbitControls (added variables projectionMatrix and modelViewMatrix into vertexShader)

--- src/scenes/components/shaders/audioVisualizer/AudioVisualizerShader1.js
+++ src/scenes/components/shaders/audioVisualizer/AudioVisualizerShader1.js
@@ -18,7 +18,7 @@ export default class AudioVisualizerShader1 extends THREE.ShaderMaterial {
 
 				vUv = uv;
 
-				gl_Position = vec4( position, 1.0 );
+				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
 			}
         `,
